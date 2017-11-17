dbd5886d0d33d179017186ada9e4f7b874dc32bb
Anthony Panozzo <panozzaj@gmail.com>
2017-11-17 17:06:59 -0500

    Commit Postgres-ified db/schema.rb
    This should help avoid diff churn after migrations.

--- db/schema.rb
+++ db/schema.rb
@@ -12,6 +12,9 @@
 
 ActiveRecord::Schema.define(version: 20170303091217) do
 
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
+
   create_table "authorizations", force: :cascade do |t|
     t.integer  "user_id"
     t.string   "provider"
@@ -20,7 +23,7 @@ ActiveRecord::Schema.define(version: 20170303091217) do
     t.string   "secret"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
-    t.index ["user_id"], name: "index_authorizations_on_user_id"
+    t.index ["user_id"], name: "index_authorizations_on_user_id", using: :btree
   end
 
   create_table "cities", force: :cascade do |t|
@@ -45,15 +48,15 @@ ActiveRecord::Schema.define(version: 20170303091217) do
     t.text     "params"
     t.datetime "created_at"
     t.datetime "updated_at"
-    t.index ["controller_name", "action_name", "ip_address"], name: "controlleraction_ip_index"
-    t.index ["controller_name", "action_name", "request_hash"], name: "controlleraction_request_index"
-    t.index ["controller_name", "action_name", "session_hash"], name: "controlleraction_session_index"
-    t.index ["impressionable_type", "impressionable_id", "ip_address"], name: "poly_ip_index"
-    t.index ["impressionable_type", "impressionable_id", "params"], name: "poly_params_request_index"
-    t.index ["impressionable_type", "impressionable_id", "request_hash"], name: "poly_request_index"
-    t.index ["impressionable_type", "impressionable_id", "session_hash"], name: "poly_session_index"
-    t.index ["impressionable_type", "message", "impressionable_id"], name: "impressionable_type_message_index"
-    t.index ["user_id"], name: "index_impressions_on_user_id"
+    t.index ["controller_name", "action_name", "ip_address"], name: "controlleraction_ip_index", using: :btree
+    t.index ["controller_name", "action_name", "request_hash"], name: "controlleraction_request_index", using: :btree
+    t.index ["controller_name", "action_name", "session_hash"], name: "controlleraction_session_index", using: :btree
+    t.index ["impressionable_type", "impressionable_id", "ip_address"], name: "poly_ip_index", using: :btree
+    t.index ["impressionable_type", "impressionable_id", "params"], name: "poly_params_request_index", using: :btree
+    t.index ["impressionable_type", "impressionable_id", "request_hash"], name: "poly_request_index", using: :btree
+    t.index ["impressionable_type", "impressionable_id", "session_hash"], name: "poly_session_index", using: :btree
+    t.index ["impressionable_type", "message", "impressionable_id"], name: "impressionable_type_message_index", using: :btree
+    t.index ["user_id"], name: "index_impressions_on_user_id", using: :btree
   end
 
   create_table "pictures", force: :cascade do |t|
@@ -64,7 +67,7 @@ ActiveRecord::Schema.define(version: 20170303091217) do
     t.integer  "place_id"
     t.string   "public_id"
     t.integer  "user_id"
-    t.index ["user_id"], name: "index_pictures_on_user_id"
+    t.index ["user_id"], name: "index_pictures_on_user_id", using: :btree
   end
 
   create_table "places", force: :cascade do |t|
@@ -82,9 +85,9 @@ ActiveRecord::Schema.define(version: 20170303091217) do
     t.integer  "followed_id"
     t.datetime "created_at",  null: false
     t.datetime "updated_at",  null: false
-    t.index ["followed_id"], name: "index_relationships_on_followed_id"
-    t.index ["follower_id", "followed_id"], name: "index_relationships_on_follower_id_and_followed_id", unique: true
-    t.index ["follower_id"], name: "index_relationships_on_follower_id"
+    t.index ["followed_id"], name: "index_relationships_on_followed_id", using: :btree
+    t.index ["follower_id", "followed_id"], name: "index_relationships_on_follower_id_and_followed_id", unique: true, using: :btree
+    t.index ["follower_id"], name: "index_relationships_on_follower_id", using: :btree
   end
 
   create_table "roles", force: :cascade do |t|
@@ -101,21 +104,21 @@ ActiveRecord::Schema.define(version: 20170303091217) do
     t.integer  "tagger_id"
     t.string   "context",       limit: 128
     t.datetime "created_at"
-    t.index ["context"], name: "index_taggings_on_context"
-    t.index ["tag_id", "taggable_id", "taggable_type", "context", "tagger_id", "tagger_type"], name: "taggings_idx", unique: true
-    t.index ["tag_id"], name: "index_taggings_on_tag_id"
-    t.index ["taggable_id", "taggable_type", "context"], name: "index_taggings_on_taggable_id_and_taggable_type_and_context"
-    t.index ["taggable_id", "taggable_type", "tagger_id", "context"], name: "taggings_idy"
-    t.index ["taggable_id"], name: "index_taggings_on_taggable_id"
-    t.index ["taggable_type"], name: "index_taggings_on_taggable_type"
-    t.index ["tagger_id", "tagger_type"], name: "index_taggings_on_tagger_id_and_tagger_type"
-    t.index ["tagger_id"], name: "index_taggings_on_tagger_id"
+    t.index ["context"], name: "index_taggings_on_context", using: :btree
+    t.index ["tag_id", "taggable_id", "taggable_type", "context", "tagger_id", "tagger_type"], name: "taggings_idx", unique: true, using: :btree
+    t.index ["tag_id"], name: "index_taggings_on_tag_id", using: :btree
+    t.index ["taggable_id", "taggable_type", "context"], name: "index_taggings_on_taggable_id_and_taggable_type_and_context", using: :btree
+    t.index ["taggable_id", "taggable_type", "tagger_id", "context"], name: "taggings_idy", using: :btree
+    t.index ["taggable_id"], name: "index_taggings_on_taggable_id", using: :btree
+    t.index ["taggable_type"], name: "index_taggings_on_taggable_type", using: :btree
+    t.index ["tagger_id", "tagger_type"], name: "index_taggings_on_tagger_id_and_tagger_type", using: :btree
+    t.index ["tagger_id"], name: "index_taggings_on_tagger_id", using: :btree
   end
 
   create_table "tags", force: :cascade do |t|
     t.string  "name"
     t.integer "taggings_count", default: 0
-    t.index ["name"], name: "index_tags_on_name", unique: true
+    t.index ["name"], name: "index_tags_on_name", unique: true, using: :btree
   end
 
   create_table "trips", force: :cascade do |t|
@@ -134,13 +137,13 @@ ActiveRecord::Schema.define(version: 20170303091217) do
     t.float    "cached_weighted_average", default: 0.0
     t.integer  "impressions_count",       default: 0
     t.boolean  "approved",                default: false
-    t.index ["cached_votes_down"], name: "index_trips_on_cached_votes_down"
-    t.index ["cached_votes_score"], name: "index_trips_on_cached_votes_score"
-    t.index ["cached_votes_total"], name: "index_trips_on_cached_votes_total"
-    t.index ["cached_votes_up"], name: "index_trips_on_cached_votes_up"
-    t.index ["cached_weighted_average"], name: "index_trips_on_cached_weighted_average"
-    t.index ["cached_weighted_score"], name: "index_trips_on_cached_weighted_score"
-    t.index ["cached_weighted_total"], name: "index_trips_on_cached_weighted_total"
+    t.index ["cached_votes_down"], name: "index_trips_on_cached_votes_down", using: :btree
+    t.index ["cached_votes_score"], name: "index_trips_on_cached_votes_score", using: :btree
+    t.index ["cached_votes_total"], name: "index_trips_on_cached_votes_total", using: :btree
+    t.index ["cached_votes_up"], name: "index_trips_on_cached_votes_up", using: :btree
+    t.index ["cached_weighted_average"], name: "index_trips_on_cached_weighted_average", using: :btree
+    t.index ["cached_weighted_score"], name: "index_trips_on_cached_weighted_score", using: :btree
+    t.index ["cached_weighted_total"], name: "index_trips_on_cached_weighted_total", using: :btree
   end
 
   create_table "users", force: :cascade do |t|
@@ -171,9 +174,9 @@ ActiveRecord::Schema.define(version: 20170303091217) do
     t.string   "instagram_url"
     t.string   "website_url"
     t.string   "blog_url"
-    t.index ["email"], name: "index_users_on_email", unique: true
-    t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
-    t.index ["role_id"], name: "index_users_on_role_id"
+    t.index ["email"], name: "index_users_on_email", unique: true, using: :btree
+    t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true, using: :btree
+    t.index ["role_id"], name: "index_users_on_role_id", using: :btree
   end
 
   create_table "votes", force: :cascade do |t|
@@ -186,8 +189,8 @@ ActiveRecord::Schema.define(version: 20170303091217) do
     t.integer  "vote_weight"
     t.datetime "created_at"
     t.datetime "updated_at"
-    t.index ["votable_id", "votable_type", "vote_scope"], name: "index_votes_on_votable_id_and_votable_type_and_vote_scope"
-    t.index ["voter_id", "voter_type", "vote_scope"], name: "index_votes_on_voter_id_and_voter_type_and_vote_scope"
+    t.index ["votable_id", "votable_type", "vote_scope"], name: "index_votes_on_votable_id_and_votable_type_and_vote_scope", using: :btree
+    t.index ["voter_id", "voter_type", "vote_scope"], name: "index_votes_on_voter_id_and_voter_type_and_vote_scope", using: :btree
   end
 
 end
